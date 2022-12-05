3e116cca00e1737c6c406f28e855a52b1062573a
IsuruPabasara <rmisur@gmail.com>
2022-12-05 14:34:38 -0800

    NA

---
+++ precache-manifest.cbad4e4dc2375936a17e9ed09295d5f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "409ca0ad7bf2ae5e4a62",
+    "url": "/static/css/main.535b270b.chunk.css"
+  },
+  {
+    "revision": "409ca0ad7bf2ae5e4a62",
+    "url": "/static/js/main.409ca0ad.chunk.js"
+  },
+  {
+    "revision": "d4634b5ed7ed2f72990d",
+    "url": "/static/js/1.d4634b5e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8caa585ea5d83929709e561e9b778aa6",
+    "url": "/index.html"
+  }
+];
