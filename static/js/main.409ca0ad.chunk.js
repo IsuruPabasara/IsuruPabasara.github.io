2702f38651ad6b3540658988e467e4d7bb7c7a4d
Rajapakshe Mudiyanselage Isuru Pabasara Rajapakshe <rrajapa@calstatela.edu>
2025-01-28 14:03:30 -0800

    Update abput me

--- static/js/main.409ca0ad.chunk.js
+++ static/js/main.409ca0ad.chunk.js
@@ -277,7 +277,7 @@
                       r.a.createElement(
                         "p",
                         null,
-                        "I am an Electrical Engineering masters student at Cal State LA. Currently I am studying the effects of repetitive Transcranial Magnetic Stimultion(rTMS) on EEG readings of people with Major Depressive Disorder(MDD). My research lab is aiming for an objective biomarker for MDD that is easily accesible, so that doctors and patients can have an easier time in identifying mental diseases. I love doing projects that broaden my understanding of technology. You can find some of my projects that I am most proud of below. Besides work, I enjoy music and also dabble in weightlifting."
+                        "I am an Electrical Engineering masters student at Cal State LA. I work for the IntelliMed lab at CSULA under the guidance of Dr. Deborah Won. Currently I am studying the effects of repetitive Transcranial Magnetic Stimultion(rTMS) on EEG readings of people with Major Depressive Disorder(MDD). I am aiming for an objective biomarker for MDD that is easily accesible, so that doctors and patients can have an easier time in identifying mental diseases. I love doing projects that broaden my understanding of technology. You can find some of my projects that I am most proud of below. Besides work, I enjoy music and also dabble in weightlifting."
                       )
                     )
                   )
